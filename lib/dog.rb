39f9a1aa708dcfa6d1ddb87679064b635904f857
Nicholas Powers <njpowers7915@gmail.com>
2018-05-04 14:44:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,17 @@ class Dog
   def name
     @name
   end
-  def name = (dog_name)
+  
+  def name=(dog_name)
     @name = dog_name
   end
+  
   def breed
     @breed
   end
-  def breed = (dog_breed)
+  
+  def breed=(dog_breed)
     @breed = dog_breed
   end
+  
 end
